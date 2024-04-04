917ecf41512773411e309f654e40f7cd781fa8bd
EmilianaManzo <emiliana.ma96@gmail.com>
2024-04-04 13:15:02 +0200

    [finish] milestone 2

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -3,6 +3,18 @@ import { store } from '../../data/store';
   export default {
     props:{
       cardObj: Object
+    },
+
+    data(){
+      return{
+        isFlagload : true
+      }
+    },
+
+    methods:{
+      getImage(image){
+        return new URL (`../../assets/img/${image}.png` , import.meta.url).href
+      }
     }
   }
 </script>
@@ -16,8 +28,14 @@ import { store } from '../../data/store';
           </div>
           <div class="flip-card-back px-2 py-3">
             <p><strong>Titolo:</strong> {{ cardObj.title || cardObj.name }} </p>
+
             <p><strong>Titolo originale:</strong> {{ cardObj.original_title || cardObj.original_name }} </p>
-            <p><strong>Lingua:</strong> {{ cardObj.original_language }} </p>
+
+            <p
+              v-if="isFlagload"
+              ><strong>Lingua:</strong> <img :src="getImage(cardObj.original_language )" @error="isFlagload = false"  alt=""> </p>
+            <p v-else><strong>Lingua:</strong> {{ cardObj.original_language  }}</p>
+
             <p><strong>Voto:</strong> {{ cardObj.vote_average }} </p>
             <p><strong>Trama:</strong> {{ cardObj.overview }} </p>
           </div>
@@ -30,5 +48,8 @@ import { store } from '../../data/store';
 <style lang="scss" scoped>
 @use '../../assets/scss/main.scss';
 
+img{
+  width: 30px;
+}
 
 </style>
